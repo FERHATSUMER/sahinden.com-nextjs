6b040bad628e575c3724d33604a696480c1ea6e8
FERHATSUMER <sumerf39@gmail.com>
2023-05-19 15:50:50 +0300

    sahibinden.com-nextjs

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,31 +1,35 @@
 import Link from "next/link";
-import "./header.css";
+import './header.css';
+import { FaSearch } from "react-icons/fa";
+
+
 
 function Header() {
-  return (
+    return (
     <div className="header-container">
-      <img
-        src="https://s0.shbdn.com/assets/images/sahibinden_logo_v13012023:a4a65c5ace0e80eac6171317531b4611.png"
-        alt="sahibinden-logo"
-      />
-      <div className="input-container">
-        <input
-          className="input"
-          placeholder="kelime,ilan no veya mağza adı ile ara "
-        />
-      </div>
-      <Link className="detayli-arama" href="/">
-        Detaylı Arama
-      </Link>
-      <div className="heaeder-rightside">
-        <div>
-          <Link href="/">Giriş Yap</Link>
-          <Link href="/">Hesap Aç</Link>
+        <img src="https://s0.shbdn.com/assets/images/sahibinden_logo_v13012023:a4a65c5ace0e80eac6171317531b4611.png"  alt="sahibinden-logo"/>
+        <div className="input-container">
+            <input 
+            className="input"
+                placeholder="kelime,ilan no veya mağza adı ile ara "
+            />
+            <FaSearch className="search-icon" />
         </div>
-        <button>Ücretsiz*Ilan Ver</button>
-      </div>
-    </div>
-  );
+        <Link className="detayli-arama" href="/">
+                Detaylı Arama
+            </Link>
+        <div className="heaeder-rightside">
+        <Link className="right-title"
+         href="/">
+                Giriş Yap
+            </Link>
+            <Link className="right-title" 
+            href="/">
+            Hesap Aç
+            </Link>
+            <button className="button">Ücretsiz*Ilan Ver</button>
+        </div>
+    </div>  );
 }
 
 export default Header;
